c17ceead90192f487f8f71e913e55cfe61b8facc
Ilia Korvigo <ilia.korvigo@gmail.com>
2019-12-19 14:40:06 +0300

    Minor improvents in regularizers

--- mbae/attention/regularizers.py
+++ mbae/attention/regularizers.py
@@ -7,66 +7,74 @@ from mbae.attention.ops import group_attentions
 
 
 class AttentionFrobeniusNorm(regularizers.Regularizer):
+    """
+    Given a batch of multi-head attention weights of shape (r*b, l_q, l_k),
+    where b is the batch size, r is the number of attention heads, l_q is the
+    query (Q) length and l_k is the key (K) length, group all attention vectors
+    corresponding to the same word in Q, and for each attention group $A$ of
+    shape (r, l_k), calculate $| A \times {A}^{T} - \alpha \cdot I |$. Here
+    $| |$ denotes the Frobenius norm (the L2 matrix norm) and $I$ denotes the
+    identity matrix of rank r.
+    """
 
-    def __init__(self, r: int, sparse: bool):
+    def __init__(self, r: int, alpha: float, lambda_: float = 1.0,
+                 epsilon: float = K.epsilon()):
+        """
+        :param r: the number of attention heads; a integer > 1
+        :param alpha: sparsity level; a float in [0, 1]
+        :param lambda_: regularisation strength; a float in (0, 1]
+        :param epsilon: the fuzz factor used in numeric expressions
+        """
+        # TODO pass epsilon
         # TODO docs
-        # TODO argument checks
+        if not (r > 1 and isinstance(r, int)):
+            raise ValueError
+        if not (0 < lambda_ <= 1 and isinstance(lambda_, float)):
+            raise ValueError
+        if not (0 <= alpha <= 1 and isinstance(alpha, float)):
+            raise ValueError
+        if not isinstance(epsilon, float):
+            raise ValueError
         self.r = r
-        self.sparse = sparse
+        self.lambda_ = lambda_
+        self.sparsity = alpha
+        self.epsilon = epsilon
 
     def __call__(self, x: KTensor) -> KTensor:
-        # TODO docs
-        return attention_regularizer(self.r, self.sparse, x)
+        """
+        :param x: a Keras tensor of multi-head attention weights. Read the
+        documentation on ops.split_heads for more details.
+        :return:
+        """
+        rb, l_q, l_k = K.int_shape(x)
+        attention_groups = group_attentions(self.r, x)
+        # flatten the batch axis to produce a tensor of [r, l_k] attention
+        # groups
+        groups = K.reshape(attention_groups, [-1, self.r, l_k])
+        # calculate $A \times $
+        self_sim = K.batch_dot(groups, groups, axes=[2, 2])
+        # subtract an identity matrix if `sparse`
+        sparsity_term = self.sparsity * K.eye(self.r) if self.sparsity else None
+        group_norms = frobenius_norm(
+            self_sim - sparsity_term if self.sparsity else self_sim,
+            axes=[1, 2]
+        )
+        # restore the batch structure
+        return self.lambda_ * K.reshape(group_norms, [-1, l_q])
 
     def get_config(self):
-        return {'r': self.r, 'sparse': self.sparse}
+        return {'r': self.r, 'sparsity': self.sparsity, 'epsilon': self.epsilon}
 
 
-def frobenius(x: KTensor, axes: t.List[int], eps=K.epsilon()):
-    return K.sqrt(K.sum(K.square(x), axis=axes)) + eps
-
-
-def attention_regularizer(r: int, sparse: bool, attention_weights: KTensor) \
-        -> KTensor:
+def frobenius_norm(x: KTensor, axes: t.List[int] = None, eps=K.epsilon()):
     """
-    For each attention group $A$  in `attention_weights` calculate
-    $| A \times {A}^{T} - I |$ if `sparse` or $| A \times {A}^{T} |$ otherwise.
-    Here $| |$ denotes the Frobenius norm (the L2 matrix norm).
+    The Frobenius (L2 matrix) norm.
+    :param x:
+    :param axes:
+    :param eps:
+    :return:
     """
-    rb, l_q, l_k = K.int_shape(attention_weights)
-    attention_groups = group_attentions(r, attention_weights)
-    # flatten the batch axis to produce a tensor of [r, l_k] attention groups
-    groups = K.reshape(attention_groups, [-1, r, l_k])
-    # calculate $A \times $
-    self_sim = K.batch_dot(groups, groups, axes=[2, 2])
-    # subtract an identity matrix if `sparse`
-    group_norms = frobenius(
-        self_sim - K.eye(r) if sparse else self_sim, axes=[1, 2]
-    )
-    # restore the batch structure
-    return K.reshape(group_norms, [-1, l_q])
-
-
-# def attention_regulariser(r: int, sparse: bool, attention_groups: KTensor) \
-#         -> KTensor:
-#     """
-#     For each attention group $A$  in `attention_groups` calculate
-#     $| A \times {A}^{T} - I |$ if `sparse` or $| A \times {A}^{T} |$ otherwise.
-#     Here $| |$ denotes the Frobenius norm (the L2 matrix norm).
-#     """
-#     shape = K.shape(attention_groups)
-#     l_q = shape[1]
-#     l_k = shape[3]
-#     # flatten the batch axis to produce a tensor of [r, l_k] attention groups
-#     groups = K.reshape(attention_groups, [-1, r, l_k])
-#     # calculate $A \times $
-#     self_sim = K.batch_dot(groups, groups, axes=[2, 2])
-#     # subtract an identity matrix if `sparse`
-#     group_norms = frobenius(
-#         self_sim - K.eye(r) if sparse else self_sim, axes=[1, 2]
-#     )
-#     # restore the batch structure
-#     return K.reshape(group_norms, [-1, l_q])
+    return K.sqrt(K.sum(K.square(x), axis=axes) + eps)
 
 
 if __name__ == '__main__':
